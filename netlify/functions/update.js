b132871c7f7d0695e5be26c06ba3d0e1723a9495
chamodisala <140143678+chamodisala@users.noreply.github.com>
2023-07-21 12:52:00 +0530

    Update update.js

--- netlify/functions/update.js
+++ netlify/functions/update.js
@@ -1,7 +1,7 @@
 const sendMessage = require("../../sendMessage");
 
 exports.handler = async (event) => {
-  console.log("Received an update from Telegram!", event.body);
+  const { message } = JSON.parse(event.body);
+  await sendMessage(message.chat.id, "I got your message!");
   return { statusCode: 200 };
 };
-
