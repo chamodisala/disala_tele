66b7a8a0f26107b1d34a3e941f28bae7c1dbb48a
chamodisala <140143678+chamodisala@users.noreply.github.com>
2023-07-21 12:44:32 +0530

    Update update.js

--- netlify/functions/update.js
+++ netlify/functions/update.js
@@ -2,34 +2,6 @@ const sendMessage = require("../../sendMessage");
 
 exports.handler = async (event) => {
   const { message } = JSON.parse(event.body);
-  const { command, botName, extra } = messageParts(message.text);
-
-  if (botName === "partiibot" || botName === null) {
-    switch (command) {
-      case "echo":
-        await sendMessage(message.chat.id, extra || "ECHO!");
-        break;
-
-      case "hashnodefeatured":
-        const { storiesFeed } = await hashnode.getFeaturedPosts();
-
-        const reply = `
-${storiesFeed[0].title} by ${storiesFeed[0].author.username}
-
-${storiesFeed[1].title} by ${storiesFeed[1].author.username}
-
-${storiesFeed[2].title} by ${storiesFeed[2].author.username}
-
-https://hashnode.com/featured
-`;
-
-        await sendMessage(message.chat.id, reply);
-        break;
-
-      default:
-        await sendMessage(message.chat.id, "I don't understand that command.");
-    }
-  }
-
+  await sendMessage(message.chat.id, "I got your message!");
   return { statusCode: 200 };
 };
