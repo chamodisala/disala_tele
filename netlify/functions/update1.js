f6de578a178603f9545cd33c3c404176b4fbec58
chamodisala <140143678+chamodisala@users.noreply.github.com>
2023-07-22 19:28:53 +0530

    Delete update1.js

--- netlify/functions/update1.js
+++
@@ -1,7 +0,0 @@
-const sendMessage = require("../../sendMessage");
-
-exports.handler = async (event) => {
-  const { message } = JSON.parse(event.body);
-  await sendMessage(message.chat.id, "I got your message!");
-  return { statusCode: 200 };
-};
