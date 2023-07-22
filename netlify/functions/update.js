151cf1bb49bef470e6bd088e786a45093b773e6a
chamodisala <140143678+chamodisala@users.noreply.github.com>
2023-07-22 19:31:41 +0530

    Update update.js

--- netlify/functions/update.js
+++ netlify/functions/update.js
@@ -6,10 +6,7 @@ exports.handler = async (event) => {
   if (message.text && message.text.trim() === '/chatid') {
     // If the user sends the "/chatid" command, send their chat ID back to them
     await sendMessage(message.chat.id, `Your Chat ID is: ${message.chat.id}`);
-  } else {
-    // If the user sends any other message, respond with a default message
-    await sendMessage(message.chat.id, "I got your message!");
-  }
+  } else 
 
   return { statusCode: 200 };
 };
